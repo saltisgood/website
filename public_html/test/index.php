4be92b33f62ea6c8727895975c31c7389a774d4e
Nicholas Stephen <git@nickstephen.com>
2014-09-23 14:21:22 +1000

    testing

---
+++ public_html/test/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick Stephen
+ * Date: 20/09/14
+ * Time: 6:36 PM
+ */
+
+echo
+"
+<!DOCTYPE html>
+<head>
+    <meta charset=\"UTF-8\" />
+    <title>Yoyo Test</title>
+
+</head>
+";
